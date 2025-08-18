130bdda3607a62cb75d5f5b309cc618806ec2a98
marifirfanibrahim <m.a.irfan.ibrahim@gmail.com>
2025-08-18 12:02:13 +0800

    update

--- model_gliner/fine_tuning/join_json.py
+++ model_gliner/fine_tuning/join_json.py
@@ -1,12 +1,15 @@
 import random
 import json
 
+ordinal_path = 'ner_news_malay\model_gliner\\fine_tuning\malay_norp.json'  # Try alternative names if needed
+norp_path = 'malay_norp.json'
+
 # ordinal
-with open('malay_ordinal_data.json', 'r', encoding='utf-8') as f:
+with open(ordinal_path, 'r', encoding='utf-8') as f:
     ordinal_data = json.load(f)
     
 # norp
-with open('malay_norp_data.json', 'r', encoding='utf-8') as f:
+with open(norp_path, 'r', encoding='utf-8') as f:
     norp_data = json.load(f)
 
 # merge & shuffle 
@@ -16,3 +19,9 @@ random.shuffle(combined_data)
 # save as json file
 with open('training_data.json', 'w', encoding='utf-8') as f:
     json.dump(combined_data, f, ensure_ascii=False, indent=2)
+
+print("JSON merged successfully!")
+print(f"Ordinal : {len(ordinal_data)}")
+print(f"NORP    : {len(norp_data)}")
+print(f"Total   : {len(combined_data)}")
+print("Saved as JSON file.")
